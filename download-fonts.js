43266490039bfa3d09f78b9883dbd12ced69116a
Oguz Karayemis <oguz.karayemis@basibozukyayincilik.org>
2026-04-01 21:14:43 +0300

    fix(fonts): Google Fonts'tan Merriweather WOFF2 dosyaları lokalize edildi

--- download-fonts.js
+++ download-fonts.js
@@ -3,7 +3,7 @@ import https from 'https';
 
 const url = 'https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&display=swap';
 
-https.get(url, { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' } }, (res) => {
+https.get(url, { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' } }, (res) => {
   let css = '';
   res.on('data', d => css += d);
   res.on('end', () => {
